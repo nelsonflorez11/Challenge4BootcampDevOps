7fe8bba649a7c5db6cf30bca204c55b638b4b37f
Nelson Florez <nelsonflorez11@gmail.com>
2022-09-08 22:32:48 -0500

    ADD: Issue #1 and 2: Crea imagen Docker 013

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:18.04
+FROM --platform=linux/amd64 ubuntu:18.04
 
 COPY install.sh .
 
@@ -9,9 +9,6 @@ WORKDIR /app
 COPY package* index.js /app/
 
 RUN npm install
-
-EXPOSE 8888
-
 
 CMD ["npm","start"]
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-const PORT = 8888;
+//const PORT = 8888;
 
 var app = express();
 
@@ -14,5 +14,9 @@ app.get('/:name', function (req, res) {
   res.send('Hola ' + req.params.name);
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+const APP_PORT = process.env.PORT;
+
+var listener = app.listen(APP_PORT, function(){
+  console.log('Listening on port ' + listener.address().port); //Listening on port APP_PORT
+});
+
